b90405225bd31d2c2453f0e32ab91149c1c31eb8
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2024-01-26 09:32:03 -0900

    sem1 added

---
+++ seminars/sem1/script.js
@@ -0,0 +1,45 @@
+let num = 123;
+console.log(`num = ${num}`);
+
+let a = 5;
+console.log(`a = ${a}`);
+a = 7;
+console.log(`a = ${a}`);
+
+let b = 1 + 2 + 3;
+console.log(`b = ${b}`);
+
+// one string comment
+
+/* many
+string
+comment */
+
+let num1 = 10;
+let num2 = 2;
+let sum = num1 + num2;
+let dif = num1 - num2;
+let mult = num1 * num2;
+let div = num1 / num2;
+
+console.log(`sum=${sum} dif=${dif} mult=${mult} div=${div}`);
+
+let aa = 1.5;
+let bb = 0.75;
+let sum2 = aa + bb;
+console.log(aa + bb, sum2);
+
+let aaa = 10;
+console.log(-aaa);
+
+const firstname = 'Nataliya';
+const lastname = 'Yadykina';
+console.log(`firstname: ${firstname}, lastname: ${lastname}`);
+
+const part1 = 'java';
+const part2 = 'script';
+console.log(part1 + part2);
+
+const part11 = 'hello';
+const part22 = 'world';
+console.log(`${part11} ${part22}`);
